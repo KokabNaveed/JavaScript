a3e1a719dffacad7da359b24f9b2de97be5a8f00
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-15 21:43:10 +0500

    Initial Commit

---
+++ 1.Variable.js
@@ -0,0 +1,7 @@
+// data type define nae krty
+const a=10;
+console.log(a);
+ a=10;
+console.log(a);
+var b="kokab";
+console.log(b);
---
+++ Helloworld.js
@@ -0,0 +1,3 @@
+console.log("Hello KOKAB");
+console.log(3+5); //this is a number
+console.log("3+5"); //this is text

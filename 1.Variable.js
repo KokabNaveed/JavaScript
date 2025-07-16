86c3639b89680e5612b144c61c8658712db6d5b1
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-16 23:40:22 +0500

    Learn var type variable

--- 1.Variable.js
+++
@@ -1,7 +0,0 @@
-// data type define nae krty
-const a=10;
-console.log(a);
- a=10;
-console.log(a);
-var b="kokab";
-console.log(b);
---
+++ 2.varVariable.js
@@ -0,0 +1,13 @@
+// data type define nae krty
+// just Let, var, Const keyword use krty 
+
+// Var -> this is used to declare variable for old browsers ... however var can be redefined in the same scope
+
+
+var name="Kokab";
+
+console.log("My name is",name);
+
+var name="Kokab Naveed"; // redefine name variable in same scope
+
+console.log("My name after redefine is",name);

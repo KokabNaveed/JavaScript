f09e394ea5432fc5eaf5d95ce37d5e518defb8d9
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-16 23:25:47 +0500

    Learn To Write output

---
+++ ConsoleLog.js
@@ -0,0 +1,17 @@
+// this is how to write output 
+
+console.log("Hello World"); // semi-colon is optional but good practice to write it 
+
+console.log("I am Kokab trying to learn JS");
+
+
+console.log(2,5);
+
+name ="Kokab";
+age =23;
+
+console.log("My name is", name ,"and i am",age,"years old."); 
+
+//comma put a space by defualt
+// the output is
+// My name is Kokab and i am 23 years old.
--- Helloworld.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello KOKAB");
-console.log(3+5); //this is a number
-console.log("3+5"); //this is text

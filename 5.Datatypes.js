c29fdcbefd518613ed7850f57d487874a2beb3c7
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-17 00:34:37 +0500

    Learn Datatypes in JS

---
+++ 5.Datatypes.js
@@ -0,0 +1,10 @@
+// js has datatypes of numbbers, strings, boolean, Null, undefined and object
+
+// but while declaring variable we don't need to specify it like int age=23; .... instead we use age = 23; ... compiler will interpert it as a number
+
+age = 23; // number
+name = "KOKAB"; // string
+IsMarried = false; // boolean
+grade='A'; // string -> char behave as string
+CGPA = 3.581 ; // number -> float behave as numbers 
+

b58b8a83a2524b4fe60d07a4d0bf149e2f3e7c72
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-17 22:38:24 +0500

    Learn conditions in js

---
+++ 7.conditions.js
@@ -0,0 +1,28 @@
+// if condition
+
+hour = 12;
+if (hour < 18) {
+  console.log("Good day");
+}
+
+// if else
+
+hour = 19;
+if (hour < 18) {
+  console.log("Good day");
+}
+else{
+   console.log("Good night");  
+}
+
+// else if
+
+time=15;
+
+if (time < 10) {
+  console.log("Good morning");
+} else if (time < 20) {
+  console.log("Good day");
+} else {
+  console.log("Good evening");
+}

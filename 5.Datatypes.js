f1ae80fc0383c8b51d179662ceaf7b26698248cc
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-17 22:12:20 +0500

    Learn Datatypes in JS

--- 5.Datatypes.js
+++ 5.Datatypes.js
@@ -7,4 +7,14 @@ name = "KOKAB"; // string
 IsMarried = false; // boolean
 grade='A'; // string -> char behaves as string
 CGPA = 3.581 ; // number -> float behaves as numbers 
+fav_subjects = ['Maths','Data Science','NLP','CVIP']; // array as an object
 
+
+// typeof function will show the datatype of variable
+
+console.log("The type of",age,"is",typeof(age));
+console.log("The type of",name,"is",typeof(name));
+console.log("The type of",IsMarried,"is",typeof(IsMarried));
+console.log("The type of",grade,"is",typeof(grade));
+console.log("The type of",CGPA,"is",typeof(CGPA));
+console.log("The type of array","is",typeof(fav_subjects));
